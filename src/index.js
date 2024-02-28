105964f5b6db8646a566ad3430b383cdafdc02e9
thatboikham <khamjapher1@gmail.com>
2024-02-28 21:45:58 +0300

    feat: add a page and related changes

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const btn = document.querySelectorAll("button");
+const content = document.getElementById("content")
+// btn.classList.add("btn")e
+// content.classList.add("btn")
+console.log(btn)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  index: [
+    './src/index.js',
+    "./src/style.css"
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+},
+};
