d1605fdaa469fb858b85f665b2f6784d42376cf5
thatboikham <khamjapher1@gmail.com>
2024-02-29 14:54:15 +0300

    feat: removed unwanted dist files
    add also bundle.js in the dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    index: [
-      './src/index.js',
-      "./src/load.js"
-    ],
-  },
+  entry: './src/index.js',
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
